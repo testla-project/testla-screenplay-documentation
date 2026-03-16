1340affe35ed0be979717407373d4005ca88027d
P.D. <munichbughunter@gmail.com>
2026-03-16 16:00:07 +0100

    Try to fix pipeline

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -16,7 +16,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://testla-screenplay.github.io',
+  url: 'https://testla-project.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: baseUrl,
