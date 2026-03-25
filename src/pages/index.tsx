de1a083c7632aadceec0de1def770b6d8d0e5242
Kenji Brückner <kenji.brueckner@gmail.com>
2026-03-25 14:12:39 +0100

    updated page metadata

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -33,8 +33,8 @@ export default function Home(): ReactNode {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title="Testla Screenplay"
+      description="Write tests your business can read">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
